6bfaf7c8312db2f489db337e711bccc4e2e3d17c
EnzoCMarini <enzocassao@gmail.com>
2025-02-20 11:26:54 -0300

    aula 1

---
+++ aula.js
@@ -0,0 +1,15 @@
+console.clear();
+/*console.log(`Enzo Cassão Marini`);
+*/
+var nome = "Enzo";
+var idade = 15;
+var tempolivre1 = "jogar";
+var tempolivre2 = "assistir videos";
+var tempolivre3 = "fazer churrasco";
+var saudacao = "Boa Tarde";
+var graduacao = "TI";
+/*
+1 - Na frase abaixo substitua _________ por sua variável que vai
+carregar a informação desejada
+*/
+console.log("".concat(saudacao, "!!\n    \n    Meu nome \u00E9 ").concat(nome, ", tenho ").concat(idade, " anos, no meu tempo\n    livre gosto de ").concat(tempolivre1, ", ").concat(tempolivre2, " e ").concat(tempolivre3, ". Tenho a inten\u00E7\u00E3o\n    de me formar em ").concat(graduacao, "."));
---
+++ aula.ts
@@ -0,0 +1,27 @@
+console.clear ()
+/*console.log(`Enzo Cassão Marini`);
+*/
+
+let nome: string = `Enzo`;
+let idade: number = 15;
+let tempolivre1: string = `jogar`;
+let tempolivre2: string = `assistir videos`;
+let tempolivre3: string = `fazer churrasco`;
+let saudacao: string = `Boa Tarde`;
+let graduacao: string = `TI`;
+
+/*
+1 - Na frase abaixo substitua _________ por sua variável que vai
+carregar a informação desejada
+
+console.log (`_________!!
+    Meu nome é _____, tenho ______ anos, no meu tempo
+    livre gosto de __________, ___________ e _________. Tenho a intenção
+    de me formar em ___________.`);
+
+*/
+
+console.log (`${saudacao}!!
+    Meu nome é ${nome}, tenho ${idade} anos, no meu tempo
+    livre gosto de ${tempolivre1}, ${tempolivre2} e ${tempolivre3}. Tenho a intenção
+    de me formar em ${graduacao}.`);
